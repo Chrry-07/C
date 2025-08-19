a3cb1c6b35c7e3517f03046aba8064873306efe2
Chrry-07 <charitha.bhanavath@gmail.com>
2025-08-19 20:17:08 +0530

    Update arOfTri.c

--- 24026/arOfTri.c
+++ 24026/arOfTri.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     int a, b, c;
-    int s;
-    int area;
+    int s, area;
 
     printf("Enter length of sides a, b, c: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    s = (a + b + c)/2;
-    area = sqrt(s *(s - a) * (s - b) * (s - c));
+    // Semi-perimeter
+    s = (a + b + c) / 2;
 
-    printf("Area of triangle using heron's formula is: %.2d\n", area);
+    // Heron's formula
+    area = sqrt(s * (s - a) * (s - b) * (s - c));
+
+    printf("Area of triangle using Heron's formula is: %d\n", area);
+
+    return 0;
 }
